97efb3278a03dad3d909ab4ffec4e2db9bbb4a96
Alain Nicolas <alain.nicolas.perso@gmail.com>
2024-12-26 19:42:02 +0100

    Display score on frontend

--- packages/dashboard/src/components/Main.tsx
+++ packages/dashboard/src/components/Main.tsx
@@ -1,7 +1,8 @@
 "use client";
-import CheckOwnership from "@/components/CheckOwnership";
 import { useAccount } from "wagmi";
+import CheckOwnership from "@/components/CheckOwnership";
 import CheckHolding from "@/components/CheckHolding";
+import CheckScore from "./CheckScore";
 
 export default function Main() {
   const { isConnected, address } = useAccount();
@@ -11,6 +12,7 @@ export default function Main() {
         <>
           <CheckOwnership address={address} />
           <CheckHolding address={address} />
+          <CheckScore address={address} />
         </>
       ) : (
         <span>Connect your wallet to get started</span>
