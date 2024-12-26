f5fae11cdb415d944e44f2dd9a5cd971f6de58de
Alain Nicolas <alain.nicolas.perso@gmail.com>
2024-12-26 22:59:21 +0100

    Remove chain selector

--- packages/dashboard/src/components/WalletButton.tsx
+++ packages/dashboard/src/components/WalletButton.tsx
@@ -1,5 +1,5 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export const WalletButton = () => {
-  return <ConnectButton showBalance={false} />;
+  return <ConnectButton showBalance={false} chainStatus={"none"} />;
 };
