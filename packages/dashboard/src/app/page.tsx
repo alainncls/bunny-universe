6d88f8fd398e765a711fcef85df4a39aea5d3dfe
Alain Nicolas <alain.nicolas.perso@gmail.com>
2025-01-01 19:46:33 +0100

    Add better metadata & OpenGraph tags

--- packages/dashboard/src/app/page.tsx
+++ packages/dashboard/src/app/page.tsx
@@ -4,8 +4,40 @@ import Main from "@/components/Main";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Bunny Universe Dashboard",
-  description: "Bunny Universe Dashboard",
+  title: "Earn Carrots | Bunny Universe",
+  description: "Keep track of your $CARROT score in the Bunny Universe!",
+  applicationName: "Earn Carrots",
+  authors: [
+    { name: "alain.linea.eth", url: "https://x.com/Alain_Ncls" },
+    {
+      name: "0xSupertramp",
+      url: "https://x.com/supertramplens",
+    },
+  ],
+  generator: "Next.js",
+  keywords: ["Bunny Universe", "Dashboard", "Carrots", "Linea", "NFT"],
+  robots: { index: true, follow: true },
+  icons: [
+    { rel: "icon", url: "https://earncarrot.bunnyuniverse.xyz/logo.png" },
+    {
+      rel: "apple-touch-icon",
+      url: "https://earncarrot.bunnyuniverse.xyz/logo.png",
+    },
+  ],
+  openGraph: {
+    type: "website",
+    url: "https://earncarrot.bunnyuniverse.xyz",
+    title: "Earn Carrots | Bunny Universe",
+    description: "Keep track of your $CARROT score in the Bunny Universe!",
+    siteName: "Earn Carrots | Bunny Universe",
+    images: [{ url: "https://earncarrot.bunnyuniverse.xyz/logo.png" }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@BunnyUniverse",
+    creator: "@Alain_Ncls",
+    images: "https://earncarrot.bunnyuniverse.xyz/logo.png",
+  },
 };
 
 export default function Home() {
