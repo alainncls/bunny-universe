9b1d45609db89e8617d558b5c9d955c8a9cd5fa8
Alain Nicolas <alain.nicolas.perso@gmail.com>
2025-01-09 17:24:24 +0100

    Improve mobile menu

--- packages/website/index.js
+++ packages/website/index.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const burgerMenu = document.querySelector('.burger-menu');
   const nav = document.querySelector('.nav');
+  const body = document.body;
 
   burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('active');
     nav.classList.toggle('active');
+    body.classList.toggle('menu-open');
   });
 
   const navLinks = nav.querySelectorAll('a');
@@ -12,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     link.addEventListener('click', () => {
       burgerMenu.classList.remove('active');
       nav.classList.remove('active');
+      body.classList.remove('menu-open');
     });
   });
 }); 
